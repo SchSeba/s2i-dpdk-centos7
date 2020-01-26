101a95631e4d1cd3e9c066318c72182e61e77146
Sebastian Sch <sebassch@gmail.com>
2020-01-26 19:18:38 +0200

    Try another commit ID latest is broken
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- test/test-app/build.sh
+++ test/test-app/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #git clone http://dpdk.org/git/apps/pktgen-dpdk
-git clone https://github.com/pktgen/Pktgen-DPDK.git
+git clone https://github.com/DPDK/dpdk.git
 # shellcheck disable=SC2164
 #cd ./pktgen-dpdk
-cd ./Pktgen-DPDK
+
+cd dpdk/examples/l2fwd
 
 make
 #cp ./app/x86_64-native-linuxapp-gcc/pktgen /usr/bin
