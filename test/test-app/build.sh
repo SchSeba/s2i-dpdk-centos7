a537655c52e50030087fb81dabdeced6f9b97cc1
Sebastian Sch <sebassch@gmail.com>
2020-01-26 19:04:59 +0200

    Update Path
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- test/test-app/build.sh
+++ test/test-app/build.sh
@@ -2,7 +2,8 @@
 #git clone http://dpdk.org/git/apps/pktgen-dpdk
 git clone https://github.com/pktgen/Pktgen-DPDK.git
 # shellcheck disable=SC2164
-cd ./pktgen-dpdk
+#cd ./pktgen-dpdk
+cd ./Pktgen-DPDK
 
 make
 #cp ./app/x86_64-native-linuxapp-gcc/pktgen /usr/bin
