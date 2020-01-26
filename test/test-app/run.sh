1fbcacbf820d088f3d7a03a6d794239154b74921
Sebastian Sch <sebassch@gmail.com>
2020-01-26 14:29:52 +0200

    create yaml for imagestream

--- test/test-app/run.sh
+++ test/test-app/run.sh
@@ -3,7 +3,7 @@
 if [ "$RUN_TYPE" == "testpmd" ]; then
 export CPU=`cat /sys/fs/cgroup/cpuset/cpuset.cpus`
 echo ${CPU}
-testpmd -l ${CPU} -w ${PCIDEVICE_OPENSHIFT_IO_SRIOVNIC} -- -i --portmask=0x1 --nb-cores=2 --forward-mode=macswap --port-topology=loop
+testpmd -l ${CPU} -w ${PCIDEVICE_OPENSHIFT_IO_DPDKNIC} -- -i --portmask=0x1 --nb-cores=2 --forward-mode=macswap --port-topology=loop
 else
   while true; do sleep 300000000; done;
 fi
