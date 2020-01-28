762cb46fb18e164845ca113c349154f91a881cb9
Sebastian Sch <sebassch@gmail.com>
2020-01-28 23:43:49 +0200

    Update the test application
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- test/test-app/build.sh
+++ test/test-app/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#git clone http://dpdk.org/git/apps/pktgen-dpdk
 git clone https://github.com/DPDK/dpdk.git
-# shellcheck disable=SC2164
-#cd ./pktgen-dpdk
 
 cd dpdk/examples/l2fwd
+#
+#make
 
-make
+echo "build done"
+
 #cp ./app/x86_64-native-linuxapp-gcc/pktgen /usr/bin
